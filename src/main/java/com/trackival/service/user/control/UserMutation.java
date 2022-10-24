962dea4b7060b61925b8230502a4bcfc114480aa
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-10-24 12:10:43 +0200

    refactor: remove unused import

--- src/main/java/com/trackival/service/user/control/UserMutation.java
+++ src/main/java/com/trackival/service/user/control/UserMutation.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 

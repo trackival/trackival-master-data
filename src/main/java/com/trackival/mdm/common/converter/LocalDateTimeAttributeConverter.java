019c4dab610ee9e72f35944b407eeb783ccbeacd
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-10-31 12:14:30 +0100

    refactor: rename parameter in LocalDateTimeAttributeConverter

--- src/main/java/com/trackival/mdm/common/converter/LocalDateTimeAttributeConverter.java
+++ src/main/java/com/trackival/mdm/common/converter/LocalDateTimeAttributeConverter.java
@@ -9,12 +9,12 @@ import java.time.LocalDateTime;
 public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
     @Override
-    public Timestamp convertToDatabaseColumn(LocalDateTime time) {
-        return time == null ? null : Timestamp.valueOf(time);
+    public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+        return attribute == null ? null : Timestamp.valueOf(attribute);
     }
 
     @Override
-    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
-        return timestamp == null ? null : timestamp.toLocalDateTime();
+    public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+        return dbData == null ? null : dbData.toLocalDateTime();
     }
 }

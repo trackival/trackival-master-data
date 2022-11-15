58884ceaa3f5f47d218eb0c5dc6b88f0472703cf
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-11-15 11:32:00 +0100

    fix: extract openFilter function from application class into CommonConfiguration

--- src/main/java/com/trackival/mdm/TrackivalServiceApplication.java
+++ src/main/java/com/trackival/mdm/TrackivalServiceApplication.java
@@ -2,24 +2,11 @@ package com.trackival.mdm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-
-import javax.servlet.Filter;
 
 @SpringBootApplication
 public class TrackivalServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrackivalServiceApplication.class, args);
-    }
-
-    /**
-     * Register the {@link OpenEntityManagerInViewFilter} so that the
-     * GraphQL-Servlet can handle lazy loads during execution.
-     */
-    @Bean
-    public Filter OpenFilter() {
-        return new OpenEntityManagerInViewFilter();
     }
 }
--- src/main/java/com/trackival/mdm/common/CommonConfiguration.java
+++ src/main/java/com/trackival/mdm/common/CommonConfiguration.java
@@ -3,7 +3,9 @@ package com.trackival.mdm.common;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
+import javax.servlet.Filter;
 import java.util.Random;
 
 @Configuration
@@ -12,5 +14,13 @@ public class CommonConfiguration {
 
     public @Bean Random random() {
         return new Random();
+    }
+
+    /**
+     * Register the {@link OpenEntityManagerInViewFilter} so that the
+     * GraphQL-Servlet can handle lazy loads during execution.
+     */
+    public @Bean Filter openFilter() {
+        return new OpenEntityManagerInViewFilter();
     }
 }
